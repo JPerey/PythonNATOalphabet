94c029dd7461009353363e07398fbd0004ef446a
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-10-29 16:31:45 -0700

    UPDATED: added exception error handling

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import pandas
 
+# TODO 1. Create a dictionary in this format:
 
-#TODO 1. Create a dictionary in this format:
 
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 alphabet_dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
-alphabet_dict = {}
+alphabet_dict = {row.letter: row.code for (index, row) in alphabet_dataframe.iterrows()}
 done = False
-
 
 while not done:
     player_word = input("Please enter a word turn phonetic: ").upper()
     player_word_split = [letter for letter in player_word]
-    player_word_length = len(player_word_split)
-    phonetic_word = []
-    i = 0
-    print(player_word_split)
-
-    for player_letter in player_word_split:
-        for (index, row) in alphabet_dataframe.iterrows():
-            if player_letter == row.letter:
-                phonetic_word.append(row.code)
-                if i < player_word_length:
-                    i += 1
-
-    print(phonetic_word)
-
-    player_choice = input("would you like to try another word? yes or no: ").lower()
+    try:
+        phonetic_word = [alphabet_dict[letter] for letter in player_word_split]
+    except KeyError as e:
+        print(f"{player_word} contains values that are not letters. Please only use letters.")
+        player_choice = input("would you like to enter a correct word? yes or no: ").lower()
+    else:
+        print(phonetic_word)
+        player_choice = input("would you like to try another word? yes or no: ").lower()
 
     if player_choice == "no":
         done = True
-
-
-
-
